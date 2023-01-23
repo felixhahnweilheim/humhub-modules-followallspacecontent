4cbc1883907a6a87fe5d59b0982435d4780b9c2d
Felix Hahn <info@hahn-felix.de>
2023-01-23 17:01:43 +0100

    Update Events.php

--- Events.php
+++ Events.php
@@ -9,10 +9,9 @@ class Events
 {
     /*
      * Callback on user joining a space
-
-     Callback on user joining a space
      *
      * Switch on the space notifications when a user is added to a space (after invitation, request or direct adding)
+     * since HumHub v1.13.1 the event is also called for the space creator
      *
      * @param MemberEvent $event
     */
